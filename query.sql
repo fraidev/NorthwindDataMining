52c65370c07fab61dd85f8fe67ac8defeaf25ab5
Frai <fraifelipe@gmail.com>
2019-11-30 03:58:28 -0300

    query

---
+++ query.sql
@@ -0,0 +1,7 @@
+﻿	
+SELECT a.product_id, b.product_id as copurchased, count(*) as times_bought_together
+FROM public.order_details AS a
+INNER JOIN public.order_details b ON a.order_id = b.order_id
+AND a.product_id != b.product_id
+GROUP BY a.product_id, b.product_id
+order by a.product_id
